3cc24d744c57e0b818a7a16082d78f8d08671978
HolaJun <ljh_0107@naver.com>
2018-09-04 03:29:47 +0900

    Planted grass.

--- test.bat
+++ test.bat
@@ -1,8 +1,9 @@
 @echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 
 echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
 
-git add .
+git add . 
 git commit -m "Planted grass."
 git push -u origin +master
 
