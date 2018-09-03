7d14c5aee9ef72026a9214634c9e662612d99d8a
HolaJun <ljh_0107@naver.com>
2018-09-04 00:07:03 +0900

    Planted grass.

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -2,6 +2,8 @@
 
 echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
 
+git init
+git remote add origin https://github.com/HolaJun/LetsGrowUpGrass.git
 git add . 
 git commit -m "Planted grass."
 git push -u origin +master
