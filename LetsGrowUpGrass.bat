b0a582a5aa8fa7b70470f4955a98a4e3cd24c8a0
HolaJun <ljh_0107@naver.com>
2018-09-04 19:57:18 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -6,7 +6,7 @@ set log=log.txt
 :LetsGrowUpGrass
 git add . >> logtest1.txt
 git commit -m "Planted grass" >> logtest2.txt
-git push -u origin +master >> logtest.3txt
+git push -u origin +master >> logtest3.txt
 echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. > log.txt
 IF EXIST %LOG% (
   exit
