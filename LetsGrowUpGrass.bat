6bfeb701be99972571a53b9858a868587997c0a5
HolaJun <ljh_0107@naver.com>
2018-09-04 20:14:22 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -1,15 +1,14 @@
 @echo off
 
-set readPushLog=
-
 :LetsGrowUpGrass
 git add .
 git commit -m "Planted grass"
 git push -u origin +master > pushlog.txt
-set /p readPushLog=<pushlog.txt
+set /p writePushLog=<pushlog.txt
 GOTO CheckGithubUpload
 
 :CheckGithubUpload
+set /p readPushLog=<pushlog.txt
 echo %readPushLog%
 pause
 
