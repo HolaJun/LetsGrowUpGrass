d4520916e2ab4673bf43035bf3cb232397d71e1c
HolaJun <ljh_0107@naver.com>
2018-09-04 20:11:37 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set readPushLog=""
+
 :LetsGrowUpGrass
 git add .
 git commit -m "Planted grass"
@@ -8,14 +10,13 @@ set /p readPushLog=<pushlog.txt
 GOTO CheckGithubUpload
 
 :CheckGithubUpload
-rem 깃헙 실행이 안된경우
+REM 깃헙 실행이 안된경우
 IF %readPushLog%=="" (
   GOTO LetsGrowUpGrass
-rem 실행된거면.
+REM 실행된거면.
 ) else (
   echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
   exit
 )
 
 GOTO LetGrowUpgrass
-pause
