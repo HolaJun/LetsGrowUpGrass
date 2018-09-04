bae96d92f10551218710ad1678bd456aa50859f3
HolaJun <ljh_0107@naver.com>
2018-09-04 20:07:59 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -1,20 +1,21 @@
 @echo off
 
-set update=no
-set log=log.txt
-
 :LetsGrowUpGrass
-git add . >> logtest1.txt
-git commit -m "Planted grass" >> logtest2.txt
-git push -u origin +master >> logtest3.txt
-echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. > log.txt
-IF EXIST %LOG% (
-  pause
-  exit
+git add .
+git commit -m "Planted grass"
+git push -u origin +master > pushlog.txt
+set /p readPushLog=<pushlog.txt
+GOTO CheckGithubUpload
+
+:CheckGithubUpload
+rem 깃헙 실행이 안된경우
+IF %readPushLog%=="" (
+  GOTO LetsGrowUpGrass
+rem 실행된거면.
 ) else (
-  goto LetsGrowUpgrass
+  echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
+  exit
 )
 
 GOTO LetGrowUpgrass
-
 pause
