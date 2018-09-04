58be7f99872a1a14ba5786df5ddc490ec46a87c5
HolaJun <ljh_0107@naver.com>
2018-09-04 19:52:44 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -1,9 +1,19 @@
 @echo off
 
-echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
+set update=no
+set log=log.txt
 
+:LetsGrowUpGrass
 git add .
-git commit -m "Planted grass."
-git push -u origin +master
+git commit -m "Planted grass"
+git push -u origin +master  
+echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
+IF EXIST %LOG% (
+  exit
+) else (
+  goto LetsGrowUpgrass
+)
+
+GOTO LetGrowUpgrass
 
 pause
