6c3a531c7f9f9d74c2d9991276402172a8b3a67f
HolaJun <ljh_0107@naver.com>
2018-09-04 20:17:05 +0900

    Planted grass

--- LetsGrowUpGrass.bat
+++ LetsGrowUpGrass.bat
@@ -9,10 +9,11 @@ GOTO CheckGithubUpload
 
 :CheckGithubUpload
 set /p readPushLog=<pushlog.txt
+REM 깃허브 업로드가 안되었을 경우(인터넷 문제 등) 계속해서 업로드 요청함.
 IF "%readPushLog%"=="" (
-  echo hihihi
+  GOTO LetsGrowUpGrass
 ) else (
-  echo 엘스다
+  echo [%DATE% %TIME:~0,8%] 잔디를 심었습니다. >> log.txt
 )
 pause
 
